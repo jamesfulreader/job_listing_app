e3d77597cd603bb08b428e163339794aea3677d9
jamesfulreader <jamesfulreader@gmail.com>
2024-07-28 19:10:28 -0600

    add data to db fetch data from the vue

--- frontend/src/components/JobListings.vue
+++ frontend/src/components/JobListings.vue
@@ -1,5 +1,24 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+let jobs = ref([])
+
+const getJobs = async () => {
+  const res = await axios.get('http://127.0.0.1:8000/api/jobs/')
+  const data = await res.data
+  jobs.value = data
+}
+
+getJobs()
+</script>
 
 <template>
-  <h1>Job Listings</h1>
+  <div v-for="job in jobs" :key="job.id">
+    {{ job.title }}
+    <br />
+    {{ job.description }}
+    <br />
+    {{ job.location }}
+    <br />
+  </div>
 </template>
