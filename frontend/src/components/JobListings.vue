11c0baac917bd7793f74ccc57803e697bb212142
jamesfulreader <jamesfulreader@gmail.com>
2024-07-29 21:29:34 -0600

    create joblisting vue pass data from joblistings

--- frontend/src/components/JobListings.vue
+++ frontend/src/components/JobListings.vue
@@ -1,7 +1,8 @@
 <script setup>
 import axios from 'axios'
 import { ref } from 'vue'
-let jobs = ref([])
+import JobListing from './JobListing.vue'
+const jobs = ref([])
 
 const getJobs = async () => {
   const res = await axios.get('http://127.0.0.1:8000/api/jobs/')
@@ -13,12 +14,14 @@ getJobs()
 </script>
 
 <template>
-  <div v-for="job in jobs" :key="job.id">
-    {{ job.title }}
-    <br />
-    {{ job.description }}
-    <br />
-    {{ job.location }}
-    <br />
-  </div>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+    </div>
+  </section>
 </template>
