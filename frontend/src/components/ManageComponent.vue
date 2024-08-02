7ba684e25511388b6ed2df8f729dde14d28ac7d6
jamesfulreader <jamesfulreader@gmail.com>
2024-08-02 16:54:26 -0600

    add handleDelete function

--- frontend/src/components/ManageComponent.vue
+++ frontend/src/components/ManageComponent.vue
@@ -1,9 +1,21 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import api from '@/services/api'
+import router from '@/router'
 
-defineProps({
+const props = defineProps({
   jobId: String,
 })
+
+const handleDelete = async () => {
+  try {
+    console.log()
+    const res = await api.delete(`/jobs/${props.jobId}/`)
+    router.push('/jobs')
+  } catch (error) {
+    console.error('error occured during delete: ', error)
+  }
+}
 </script>
 <template>
   <div class="bg-white p-6 rounded-lg shadow-md mt-6">
@@ -14,6 +26,7 @@ defineProps({
       >Edit Job</RouterLink
     >
     <button
+      @click.prevent="handleDelete"
       class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
     >
       Delete Job
